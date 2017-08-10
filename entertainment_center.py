e206b4728d341e3f99d9861822680af7478d41fc
Dan <danielross4242@gmail.com>
2017-08-10 18:48:53 -0500

    Cleaned up code in entertainment_center.py and media.py to conform to Pep8 standards

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,36 +1,31 @@
 import media
 import fresh_tomatoes
 
+
 # define the instances of class "movie"
 clue = media.Movie("Clue",
-                   "Six guests are invited to a strange house and must cooperate with the staff to solve a murder mystery",
-                   "https://dfep0xlbws1ys.cloudfront.net/thumbs1b/37/1b3768db2a5ca1b5c652e934cab43a28.jpg?response-cache-control=max-age=2628000",
+                   "https://dfep0xlbws1ys.cloudfront.net/thumbs1b/37/1b3768db2a5ca1b5c652e934cab43a28.jpg?response-cache-control=max-age=2628000",  # NOQA
                    "https://www.youtube.com/watch?v=nkwqw4lPz64")
 
 wet_hot = media.Movie("Wet Hot American Summer",
-                     "Set on the last day of camp, in the hot summer of 1981, a group of counselors try to complete their unfinished business before the day ends",
-                     "https://upload.wikimedia.org/wikipedia/en/5/59/Wet_hot_american_summer.jpg",
+                     "https://upload.wikimedia.org/wikipedia/en/5/59/Wet_hot_american_summer.jpg",  # NOQA
                      "https://www.youtube.com/watch?v=tIvN94hkB1A")
 
 rocky_horror = media.Movie("Rocky Horror Picture Show",
-                          "A newly engaged couple have a breakdown in an isolated area and must pay a call to the bizarre residence of Dr. Frank-N-Furter", 
-                          "https://upload.wikimedia.org/wikipedia/en/c/c2/Original_Rocky_Horror_Picture_Show_poster.jpg",
+                          "https://upload.wikimedia.org/wikipedia/en/c/c2/Original_Rocky_Horror_Picture_Show_poster.jpg",  # NOQA
                           "https://www.youtube.com/watch?v=Pgx1QZFNMz8",)
 
 prisoner = media.Movie("Harry Potter and the Prisoner of Azkaban",
-                       "It's Harry's third year at Hogwarts; not only does he have a new Defense Against the Dark Arts teacher, but there is also trouble brewing. Convicted murderer Sirius Black has escaped the Wizards' Prison and is coming after Harry",
-                       "https://upload.wikimedia.org/wikipedia/en/b/bc/Prisoner_of_azkaban_UK_poster.jpg",
+                       "https://upload.wikimedia.org/wikipedia/en/b/bc/Prisoner_of_azkaban_UK_poster.jpg",  # NOQA
                        "https://www.youtube.com/watch?v=lAxgztbYDbs")
 
 room = media.Movie("The Room",
-                   "Johnny is a successful banker who lives happily in a San Francisco townhouse with his fiance, Lisa. One day, inexplicably, she gets bored with him and decides to seduce his best friend, Mark. From there, nothing will be the same again",
-                   "https://upload.wikimedia.org/wikipedia/en/e/e1/TheRoomMovie.jpg",
+                   "https://upload.wikimedia.org/wikipedia/en/e/e1/TheRoomMovie.jpg",  # NOQA
                    "https://www.youtube.com/watch?v=tfMTHIwTUXA")
 
 angry_men = media.Movie("12 Angry Men",
-                       "A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence",
-                       "https://upload.wikimedia.org/wikipedia/en/9/91/12_angry_men.jpg",
-                       "https://youtu.be/EqDd06GW76o?list=PLZbXA4lyCtqoKE2fbNgzoaRkpMSHnzE60")
+                       "https://upload.wikimedia.org/wikipedia/en/9/91/12_angry_men.jpg",  # NOQA
+                       "https://youtu.be/EqDd06GW76o?list=PLZbXA4lyCtqoKE2fbNgzoaRkpMSHnzE60")  # NOQA
 
 # choose which instances will be displayed in variable "movies"
 movies = [clue, wet_hot, rocky_horror, prisoner, room, angry_men]
--- media.py
+++ media.py
@@ -1,14 +1,16 @@
 import webbrowser
 
-# define class "movie"
+
+# define class Movie
 class Movie():
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    # initialize class Movie
+    # with inputs movie_title et al from instances
+    # defined in entertainment_center.py
+    def __init__(self, movie_title, poster_image, trailer_youtube):
         self.title = movie_title
-        self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    # define function to show trailer
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-        
